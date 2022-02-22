ef84cbc9b2227b16bbf0e0c0960324f04d1deb86
unknown <eudi.rincon.lopez@gmail.com>
2022-02-22 14:14:27 -0400

    calcudora-eudimarys

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <footer class="py-5">
+      <div class="row">
+        <div class="col-2">
+          <h5>Estudiante</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2">
+              <a class="nav-link p-0 text-muted">Eudimarys Rincón</a>
+            </li>
+          </ul>
+        </div>
+          <a href="http://www.unefa.edu.ve/portal/" class="nav-link p-0 text-muted">© 2022 UNEFA TÁCHIRA </a>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+<style scoped>
+.container{
+background: #654ea3;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #eaafc8, #654ea3);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #eaafc8, #654ea3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+</style>
+
+
+
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <header
+      class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
+    >
+      <a
+        href="/"
+        class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+      >
+        <span class="fs-4">VSL Calculator</span>
+      </a>
+
+    </header>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style scoped>
+.container{
+background: #654ea3;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #eaafc8, #654ea3);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #eaafc8, #654ea3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+</style>
